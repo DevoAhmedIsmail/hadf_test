26d8d41b74c4bc3a1a53c6371248e4f971ba7faa
Ahmed ismail <devo.ahmedismail@gmail.com>
2023-02-09 12:06:27 +0200

    home to en

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ const menu = document.querySelector('#menu');
 button.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
+
+// to allocate the price in different direction AR or EN
+let c_price_All = [...document.querySelectorAll('.courses .Price_c')]
+if(document.dir === 'ltr') {
+  c_price_All.map(c_price=>(
+    c_price.style.right = '18px'
+  ))
+} else {
+  c_price_All.map(c_price=>(
+    c_price.style.left = '18px'
+  ))
+}
